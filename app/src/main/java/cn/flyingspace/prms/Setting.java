01fc6bc255e50b51d0686699c9085c0e9225b06b
yuchenfei <yuchenfei19950319@gmail.com>
2018-06-19 11:08:08 +0800

    更新服务器url

--- app/src/main/java/cn/flyingspace/prms/Setting.java
+++ app/src/main/java/cn/flyingspace/prms/Setting.java
@@ -1,8 +1,7 @@
 package cn.flyingspace.prms;
 
 public final class Setting {
-    public final static String SERVER_URL = "http://192.168.10.10:8000";
-//    public final static String SERVER_URL = "http://prms.flyingspace.cn:8000";
+    public final static String SERVER_URL = "http://prms-staging.flyingspace.cn";
 
     public final static String API_AUTH = SERVER_URL + "/api/auth";
     public final static String API_ITEMS = SERVER_URL + "/api/check_in/items";
